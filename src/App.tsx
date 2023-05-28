5cdf427266bd8714e138d6b49bb91717561645c6
Jatin <jatin208015@gmail.com>
2023-05-28 15:07:46 +0000

    Added basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react"
+import Score from "./components/Score"
+import TextBox from "./components/TextBox"
+import { useDispatch, useSelector } from "react-redux"
+import { initTest } from "./state/testSlice"
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const inputRef = useRef<HTMLInputElement>(null);
 
+  const handleClick = () => {
+    dispatch(initTest());
+    inputRef.current?.focus();
+  }
+
+  useEffect(() => { dispatch(initTest()); }
+    , [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="main">
+      <Navbar inputRef={inputRef} />
+      <section className="main-section">
+        <Score onClick={handleClick}/>
+        <TextBox inputRef={inputRef} />
+      </section>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+import testSlice from './state/testSlice.ts'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: {
+    test: testSlice
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
